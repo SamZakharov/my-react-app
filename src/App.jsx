1000c672005276f0fd0d77b64d313a46569f6297
SamZakharov <semen.zaharovv@gmail.com>
2024-04-29 15:17:32 +0300

    HW 84 is finished

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Card from "./components/Card.jsx";
+
+class App extends React.Component{
+    render() {
+        return (
+            <div className="App">
+                <Card />
+            </div>
+        )
+    }
+}
+
+export default App;
